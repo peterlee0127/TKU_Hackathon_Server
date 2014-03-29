1edd8f60a38b0e5e65261fe7087cc4fefa9521df
hwchain <munga0923@gmail.com>
2014-03-29 18:53:04 +0800

    new class succeed

--- public/javascripts/addNewClass.js
+++ public/javascripts/addNewClass.js
@@ -1,24 +1,44 @@
-function add_Class()
-{
+function do_POST()
+{   
     var name = $('#name').val();
     var room = $('#room').val();
-    var time = $('#time').val();    
+    var time = $('#time').val();
     var data = $('<tr>'+
-                            '<td>'+ name +'</td>'+
-                            '<td>'+ room +'</td>'+
-                            '<td>'+ time +'</td>'+
-                    '</tr>');
-    $('tbody').append(data);
-    
+                    '<td>'+ name +'</td>'+
+                    '<td>'+ room +'</td>'+
+                    '<td>'+ time +'</td>'+
+                '</tr>');
     $.ajax({
         type: "POST",
         url: "http://localhost:3000/api/newClass",
         data: "&class_name="+name+"&class_room="+room+"&class_time="+time,
-        dataType:'json'
+        dataType:'json',
+        success:function(json){
+                    var name = json.class_name;
+                    var room = json.class_room;
+                    var time = json.class_time;
+                    var data = $('<tr>'+
+                            '<td>'+ '<a href="/'+json._id+'">'+name +'</a></td>'+
+                            '<td>'+ room +'</td>'+
+                            '<td>'+ time +'</td>'+
+                    '</tr>');
+                    $('tbody').append(data);
+        }
     });
 }
-
-
+//
+//function add_class()
+//{
+//    var name = $('#name').val();
+//    var room = $('#room').val();
+//    var time = $('#time').val();  
+//    var data = $('<tr>'+
+//                            '<td>'+ name +'</td>'+
+//                            '<td>'+ room +'</td>'+
+//                            '<td>'+ time +'</td>'+
+//                    '</tr>');
+//    $('tbody').append(data);
+//}
 
 function init_class_list(json)
 {
